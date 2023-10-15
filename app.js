40e0cdcd49845051ce16c14d2e45a1cffd166759
omprakash lamichhane <omprakash lamichhane>
2023-10-15 08:47:05 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+let a=10;
+let b=20;
+console.log(a);
+console.log(b);
+console.log("hello i am omprakash lamichhane:",a+b);
+
+let subject=["physics","iit","math","digital logic","c programming"];
+
+for(let i=0;i<subject.length;i++)
+{
+    console.log("the order is",i,subject[i]);
+}
